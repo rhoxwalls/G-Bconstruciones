4eabe304f343352391a0d09bc6ca1af8d2a852e0
Ramon <edraaz199123@gmail.com>
2026-08-08 15:40:30 -0300

    remodelation

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import logo from '../assets/web.jpg';
 
 export const Nav = () => {
